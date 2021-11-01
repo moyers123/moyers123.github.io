2fcdfae0530b1908f3120da40c8875afadf9f336
moyers123 <eric.d.moy@gmail.com>
2021-11-01 14:23:49 -0700

    js isnt getting read

--- Test stuff/Sandbox/js/analogClock.js
+++ Test stuff/Sandbox/js/analogClock.js
@@ -5,6 +5,10 @@ const hourHand = document.querySelector(['data-hour-hand']);
 const minuteHand = document.querySelector(['data-minute-hand']);
 const secondHand = document.querySelector(['data-second-hand']);
 
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360);
+}
+
 function setClock() {
     const currentDate = new Date();
     const secondsRatio = (currentDate.getSeconds() / 60);
@@ -15,8 +19,5 @@ function setClock() {
     setRotation(hourHand, hoursRatio);
 }
 
-function setRotation(element, rotationRatio) {
-    element.style.setProperty('--rotation', rotationRatio * 360);
-}
 
 setClock();
