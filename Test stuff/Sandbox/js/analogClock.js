684d00dcb492110d6e127a973b6a002e9e77d476
moyers123 <eric.d.moy@gmail.com>
2021-11-01 14:26:22 -0700

    this was the error

--- Test stuff/Sandbox/js/analogClock.js
+++ Test stuff/Sandbox/js/analogClock.js
@@ -5,9 +5,9 @@ const hourHand = document.querySelector(['data-hour-hand']);
 const minuteHand = document.querySelector(['data-minute-hand']);
 const secondHand = document.querySelector(['data-second-hand']);
 
-function setRotation(element, rotationRatio) {
+/*function setRotation(element, rotationRatio) {
     element.style.setProperty('--rotation', rotationRatio * 360);
-}
+}*/
 
 function setClock() {
     const currentDate = new Date();
