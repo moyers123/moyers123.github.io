fa63deb9171b4c5cc9e2c045f39fc532ff0ac8c4
moyers123 <eric.d.moy@gmail.com>
2021-11-01 11:48:10 -0700

    Javascript functionality

---
+++ Test stuff/Sandbox/externalJS/analogClock.js
@@ -0,0 +1,22 @@
+setInterval(setClock, 1000)
+
+//document.querySelector will target the attribute in the html (html src is linked to this JS sheet)//
+const hourHand = document.querySelector(['data-hour-hand']);
+const minuteHand = document.querySelector(['data-minute-hand']);
+const secondHand = document.querySelector(['data-second-hand']);
+
+function setClock() {
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360);
+}
+
+setClock();
