1e8eeafc5b81bb0e06806daae35ccdb82105676f
moyers123 <eric.d.moy@gmail.com>
2021-11-01 12:57:28 -0700

    needs to specify date more?

--- Test stuff/Sandbox/externalJS/analogClock.js
+++ Test stuff/Sandbox/externalJS/analogClock.js
@@ -1,12 +1,12 @@
 setInterval(setClock, 1000)
 
 //document.querySelector will target the attribute in the html (html src is linked to this JS sheet)//
-const hourHand = document.querySelector(['#data-hour-hand']);
-const minuteHand = document.querySelector(['#data-minute-hand']);
-const secondHand = document.querySelector(['#data-second-hand']);
+const hourHand = document.querySelector(['data-hour-hand']);
+const minuteHand = document.querySelector(['data-minute-hand']);
+const secondHand = document.querySelector(['data-second-hand']);
 
 function setClock() {
-    const currentDate = new Date();
+    const currentDate = new Date(hours, minutes, seconds);
     const secondsRatio = currentDate.getSeconds() / 60;
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
     const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
