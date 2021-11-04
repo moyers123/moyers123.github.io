68b66e71b27d4c4e1c4cc16b74513c0b9d420fb0
moyers123 <eric.d.moy@gmail.com>
2021-11-04 11:54:09 -0700

    adding some js

---
+++ Test stuff/Sandbox/js/dropDown.js
@@ -0,0 +1,15 @@
+document.addEventListener('click', e => {
+    const isDropDownButton = e.target.matches('[data-dropdown-button]')
+    if (!isDropDownButton && e.target.closest('[data-dropdown]') != null) return;
+
+    let currentDropDown
+    if (isDropDownButton) {
+        currentDropDown = e.target.closest('[data-dropdown]')
+        currentDropDown.classList.toggle('active')
+    }
+
+    document.querySelectorAll('[data-dropdown].active').forEach(dropDown => {
+        if (dropDown === currentDropDown) return
+        dropDown.classList.remove('active')
+    })
+})
