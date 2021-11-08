34a51b8e6f8e35169461c9c83e9b3c07d122b73d
moyers123 <eric.d.moy@gmail.com>
2021-11-08 12:31:46 -0800

    variables to use

--- Test stuff/Sandbox/js/calculator.js
+++ Test stuff/Sandbox/js/calculator.js
@@ -1,2 +1,7 @@
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
+const allClearButton = document.querySelectorAll('[data-AC]');
+const equalsButton = document.querySelectorAll('[data-equals]');
+const deleteButton = document.querySelectorAll('[data-delete]');
+const previousTextElement = document.querySelectorAll('[data-previous]');
+const currentTextElement = document.querySelectorAll('[data-current]');
