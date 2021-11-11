bc9f02429006ef3318c6444fc592e3ad7afad8f1
moyers123 <eric.d.moy@gmail.com>
2021-11-11 09:55:16 -0800

    clicking power

---
+++ Test stuff/Sandbox/js/ticTacToe.js
@@ -0,0 +1,23 @@
+const x_class = 'x';
+const o_class = 'o';
+const cellElements = document.querySelectorAll(['data-cell']);
+let oTurn;
+
+cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, {once: true })
+})
+
+function handleClick(event) {
+    const cell = event.target;
+    const currentClass = oTurn ? o_class : x_class;
+    placeMark(cell, currentClass)
+    swapTurns()
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+    oTurn = !oTurn
+}
