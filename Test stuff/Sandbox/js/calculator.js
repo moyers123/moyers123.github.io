b4e3db4ce969fa3301193997956600508c0b8b09
Eric Moy <eric.d.moy@gmail.com>
2021-11-15 08:22:56 -0500

    change conditional target

--- Test stuff/Sandbox/js/calculator.js
+++ Test stuff/Sandbox/js/calculator.js
@@ -61,7 +61,7 @@ class Calculator{
         const integerDigits = parseFloat(stringNumber.split('.')[0])
         const decimalDigits = stringNumber.split('.')[1]
         let integerDisplay
-        if(isNaN(integerDisplay)) {
+        if(isNaN(integerDigits)) {
             integerDisplay = '';
         } else {
             integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
