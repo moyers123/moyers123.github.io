76caec53c33d75c28ad3952a3b1802a536973488
Eric Moy <eric.d.moy@gmail.com>
2021-12-01 12:18:19 -0800

    flip function

--- Test stuff/Sandbox/js/memory.js
+++ Test stuff/Sandbox/js/memory.js
@@ -14,22 +14,38 @@ class mixOrMatch {
     }
     startGame() {
         this.cardToCheck = null;
+        this.totalClicks = 0;
+        this.timeRemaining = this.totalTime;
+        this.matchedCards = [];
+        this.busy = true;
+    }
+    flipCard(cards) {
+        if(this.canFlipCard(cards)) {
+            this.totalClicks++;
+            this.ticker.innerText = this.totalClicks;
+            cards.classList.add('visible');
+        }    
+    }
+
+    canFlipCard(cards) {
+        return !this.busy && !this.matchedCards.includes(card) && card !== this.cardtoCheck;
     }
 }
 
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
     let card = Array.from(document.getElementsByClassName('cards'));
+    let game = new mixOrMatch(100, card);
 
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
-            //game.startGame();//
+            game.startGame();
         });
     });
     card.forEach(cards => {
         cards.addEventListener('click', () => {
-            //game.flipCard(cards);//
+            game.flipCard(cards);
         });
     });
 }
