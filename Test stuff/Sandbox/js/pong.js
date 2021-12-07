efa713081149d7bfd913a9d5a98d1af64bf1c3c0
Eric Moy <eric.d.moy@gmail.com>
2021-12-07 11:28:03 -0800

    scoring

--- Test stuff/Sandbox/js/pong.js
+++ Test stuff/Sandbox/js/pong.js
@@ -2,8 +2,10 @@ import Ball from './Pong Classes/pongBall.js';
 import Paddle from './Pong Classes/pongPaddle.js';
 
 const ball = new Ball(document.getElementById('ball'));
-const playerPaddle = new Paddle(document.getElementById('p1-paddle'));
+const player1Paddle = new Paddle(document.getElementById('p1-paddle'));
 const player2Paddle = new Paddle(document.getElementById('p2-paddle'));
+const player1Score = document.getElementById('p1-score');
+const player2Score = document.getElementById('p2-score');
 
 let lastTime
 function update(time) {
@@ -13,7 +15,7 @@ function update(time) {
         player2Paddle.update(delta, ball.y)
 
         if(lose()) {
-
+            handleLose();
         }
     }
 
@@ -26,9 +28,21 @@ function lose() {
     return rect.right >= window.innerWidth || rect.left <= 0
 }
 
+function handleLose() {
+    const rect = ball.rect();
+    if (rect.right >= window.innerWidth) {
+        player1Score.textContent = parseInt(player1Score.textContent) + 1;
+    } else {
+        player2Score.textContent = parseInt(player2Score.textContent) + 1;
+    }
+    ball.reset();
+    player2Paddle.reset();
+    player1Paddle.reset();
+}
+
 document.addEventListener('mousemove', e => {
     //if (event.key === 'w') {}//
-    playerPaddle.position = (e.y / window.innerHeight) * 100;
+    player1Paddle.position = (e.y / window.innerHeight) * 100;
 })
 
 window.requestAnimationFrame(update);
