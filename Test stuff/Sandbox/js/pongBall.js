6451fe1dd203ea851f91807f358493afd0b8b548
Eric Moy <eric.d.moy@gmail.com>
2021-12-07 10:41:56 -0800

    creating starting random direction

--- Test stuff/Sandbox/js/pongBall.js
+++ Test stuff/Sandbox/js/pongBall.js
@@ -1,6 +1,7 @@
 export default class Ball {
     constructor(ballElement) {
         this.ballElement = ballElement;
+        this.reset();
     }
 
     get x() {
@@ -17,6 +18,16 @@ export default class Ball {
 
     set y() {
         this.ballElement.style.setProperty('--y', value)
+    }
+
+    reset() {
+        this.x = 50;
+        this.y = 50;
+        this.direction = {x: 0};
+        while (Math.abs(this.direction.x) <= .2 || Math.abs(this.direction.x) >= .9) {
+            const heading = randomNumberBetween(0, 2 * Math.PI);
+            this.direction = {x: Math.cos(heading), y: Math.sin(heading) }
+        }
     }
 
     update(delta) {
