9ef43fbf2fc62b05daab6e6975616a32294108df
Eric Moy <eric.d.moy@gmail.com>
2021-12-07 10:35:36 -0800

    change positions

--- Test stuff/Sandbox/js/pong.js
+++ Test stuff/Sandbox/js/pong.js
@@ -2,8 +2,13 @@ import Ball from './pongBall.js';
 
 const ball = new Ball(document.getElementById('ball'));
 
+let lastTime
 function update(time) {
-    
+    if (lastTime != null) {
+        const delta = time - lastTime;
+    }
+
+    lastTime = time;
     window.requestAnimationFrame(update);
 }
 
--- Test stuff/Sandbox/js/pongBall.js
+++ Test stuff/Sandbox/js/pongBall.js
@@ -2,4 +2,24 @@ export default class Ball {
     constructor(ballElement) {
         this.ballElement = ballElement;
     }
+
+    get x() {
+        return parseFloat(getComputedStyle(this.ballElement).getPropertyValue('--x'));
+    }
+
+    set x() {
+        this.ballElement.style.setProperty('--x', value)
+    }
+
+    get y() {
+        return parseFloat(getComputedStyle(this.ballElement).getPropertyValue('--y'));
+    }
+
+    set y() {
+        this.ballElement.style.setProperty('--y', value)
+    }
+
+    update(delta) {
+
+    }
 }
