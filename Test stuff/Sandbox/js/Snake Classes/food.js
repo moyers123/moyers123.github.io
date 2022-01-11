e9a44023bba8341656a2ff456b9908656f7bc8da
Eric Moy <eric.d.moy@gmail.com>
2022-01-11 12:49:08 -0800

    finish snake

--- Test stuff/Sandbox/js/Snake Classes/food.js
+++ Test stuff/Sandbox/js/Snake Classes/food.js
@@ -1,28 +1,28 @@
 import { onSnake, expandSnake } from './game.js'
 import { randomGridPosition } from './grid.js'
 
-let food = getRandomFoodPosition();
-const expansionRate = 1;
+let food = getRandomFoodPosition()
+const expansionRate = 1
 
 export function update() {
-   if (onSnake(food)) {
-       expandSnake(expansionRate)
-       food = getRandomFoodPosition();
-   }
+  if (onSnake(food)) {
+    expandSnake(expansionRate)
+    food = getRandomFoodPosition()
+  }
 }
 
 export function draw(gameBoard) {
-    const foodElement = document.createElement('div')
-    foodElement.style.gridRowStart = food.y;
-    foodElement.style.gridColumnStart = food.x;
-    foodElement.classList.add('food');
-    gameBoard.appendChild(foodElement);
+  const foodElement = document.createElement('div')
+  foodElement.style.gridRowStart = food.y
+  foodElement.style.gridColumnStart = food.x
+  foodElement.classList.add('food')
+  gameBoard.appendChild(foodElement)
 }
 
 function getRandomFoodPosition() {
-    let newFoodPosition;
-    while (newFoodPosition == null || onSnake(newFoodPosition)) {
-        newFoodPosition = randomGridPosition();
-    }
-    return newFoodPosition;
+  let newFoodPosition
+  while (newFoodPosition == null || onSnake(newFoodPosition)) {
+    newFoodPosition = randomGridPosition()
+  }
+  return newFoodPosition
 }
--- Test stuff/Sandbox/js/snake.js
+++ Test stuff/Sandbox/js/snake.js
@@ -2,43 +2,43 @@ import { update as updateSnake, draw as drawSnake, snakeSpeed, getSnakeHead, sna
 import { update as updateFood, draw as drawFood} from './Snake Classes/food.js'
 import { outsideGrid } from './Snake Classes/grid.js'
 
-let lastRenderTime = 0;
-let gameOver = false;
+let lastRenderTime = 0
+let gameOver = false
 const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
-    if(gameOver) {
-        if(confirm('You lost. Press ok to restart.')) {
-            window.location = '/'
-        }
-        return
+  if (gameOver) {
+    if (confirm('You lost. Press ok to restart.')) {
+      window.location = '/Test stuff/snake.html'
     }
+    return
+  }
 
-    window.requestAnimationFrame(main);
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
-    if (secondsSinceLastRender < 1 / snakeSpeed) return
+  window.requestAnimationFrame(main)
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondsSinceLastRender < 1 / snakeSpeed) return
 
-    
-    lastRenderTime = currentTime;
 
-    update()
-    draw()
+  lastRenderTime = currentTime
+
+  update()
+  draw()
 }
 
-window.requestAnimationFrame(main);
+window.requestAnimationFrame(main)
 
 function update() {
-    updateSnake();
-    updateFood();
-    checkDeath();
+  updateSnake()
+  updateFood()
+  checkDeath()
 }
 
 function draw() {
-    gameBoard.innerHTML = '';
-    drawSnake(gameBoard);
-    drawFood(gameBoard);
+  gameBoard.innerHTML = ''
+  drawSnake(gameBoard)
+  drawFood(gameBoard)
 }
 
 function checkDeath() {
-    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
